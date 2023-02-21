de52469001c41c3db913c28633b5e8e8722b934f
Rufi <68145072+Rufi-git@users.noreply.github.com>
2023-02-21 22:55:04 +0200

    css

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/sass/main.scss', 'resources/css/main.css','resources/js/app.js', 'resources/sass/create.scss','resources/css/create.css'],
+            input: ['resources/sass/main.scss', 'resources/sass/main.css','resources/js/app.js', 'resources/sass/create.scss','resources/sass/create.css'],
             refresh: true,
         }),
         
