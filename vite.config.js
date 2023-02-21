ce1347e0d3c123795eef34f158f489a8a6fc8d41
Rufi <68145072+Rufi-git@users.noreply.github.com>
2023-02-21 09:45:09 +0200

    init

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,6 @@ export default defineConfig({
             input: ['resources/sass/register.scss', 'resources/sass/main.scss','resources/css/main.css', 'resources/js/app.js', 'resources/sass/create.scss'],
             refresh: true,
         }),
+        
     ],
 });
