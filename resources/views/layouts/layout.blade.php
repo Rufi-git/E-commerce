9870532dbcd44b4bf8d93107e2ec596915bbdd8f
Rufi <68145072+Rufi-git@users.noreply.github.com>
2023-02-22 15:26:35 +0200

    asdasdsa

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -10,12 +10,9 @@
         
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-        @vite([
-            'public/build/assets/create-793219b8.css',
-            'public/build/assets/app-575acb37.js',
-        'public/build/assets/main-e3b314ba.css'   
-    ])
+        <link rel="stylesheet" type="text/css" href="{{ asset('build/assets/create-793219b8.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('build/assets/main-e3b314ba.css') }}">
+        <script src="{{asset('build/assets/app-575acb37.js')}}"></script>
     </head>
     <body>
         @yield("products")
